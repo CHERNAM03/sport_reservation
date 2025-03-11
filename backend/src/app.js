5ab65504681c2956970900a29c9fa663bec3c271
ameerchand30 <ameerchand30@gmail.com>
2025-03-11 12:09:39 +0100

    docker files are ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const databaseConfig = require('./config/database');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,13 +12,33 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Add Hello World route
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Hello World </title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+                <p>Welcome to the cheikh App</p>
+            </body>
+        </html>
+    `);
+});
+
 // Database connection
-databaseConfig.connect();
+// databaseConfig.connect();
 
 // Routes
-app.use('/api', routes);
+// app.use('/api', routes);
 
 // Start the server
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as ReservationForm } from './ReservationForm';
-export { default as SportCard } from './SportCard';
