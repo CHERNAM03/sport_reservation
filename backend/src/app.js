4cc6bde14f25b4d35b308ebca6975ee4250da020
ameerchand30 <ameerchand30@gmail.com>
2025-03-17 16:19:49 +0100

    add ground is done

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('api/ground', groundRouter);
+app.use('/api/ground', groundRouter);
 
 // Add Hello World route
 app.get('/', (req, res) => {
--- backend/src/routes/groundRoutes.js
+++ backend/src/routes/groundRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addTerrain } = require('../controllers/groundController');
+const  { addTerrain }  = require('../controllers/groundController');
 
 // Route to add a new terrain
 router.post('/addGround', addTerrain);
